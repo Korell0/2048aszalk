ce050f0372c85fb10b2d884575ada06a2e689d21
beretkag <beretkag@gmail.com>
2022-09-14 09:09:58 +0200

    map generate

--- 2048/2048/Grid.cs
+++ 2048/2048/Grid.cs
@@ -8,5 +8,39 @@ namespace _2048
 {
     class Grid
     {
+        public int[,] tabla;
+
+        public Grid(int meret)
+        {
+            tabla = new int[meret, meret];
+            for (int sor = 0; sor < meret; sor++)
+            {
+                for (int oszlop = 0; oszlop < meret; oszlop++)
+                {
+                    tabla[sor, oszlop] = 0;
+                }
+            }
+
+            Random rand = new Random();
+
+            int[] poz1 = new int[] { rand.Next(0, meret) , rand.Next(0, meret) };
+            int[] poz2 = new int[] { poz1[0], poz1[1] };
+            while (poz1[0] == poz2[0] && poz1[1] == poz2[1])
+            {
+                poz2 = new int[] {rand.Next(0, meret) , rand.Next(0, meret) };
+            }
+
+            tabla[poz1[0], poz1[1]] = rand.Next(1, 3) * 2;
+            tabla[poz2[0], poz2[1]] = rand.Next(1, 3) * 2;
+
+
+
+        }
+
+
+
+
+
+
     }
 }
